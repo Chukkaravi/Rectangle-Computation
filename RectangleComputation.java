09c2c33a0e6f07e5fbccf41b6e0eea2297f75932
Chukka Venkata Ravi <102050741+Chukkaravi@users.noreply.github.com>
2023-01-11 22:38:29 +0530

    Add files via upload

---
+++ RectangleComputation.java
@@ -0,0 +1,11 @@
+public class RectangleComputation{
+    public static void main(String args[]){
+        double length, area, width, perimeter;
+        length = 2.344;
+        width =6.344;
+        area = length * width;
+        perimeter = 2*(length + width);
+        System.out.println("The Area is " + area);
+        System.out.println("The perimeter is " + perimeter);
+    }
+}
